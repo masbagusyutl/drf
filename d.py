68a8defa108fc548ffed4480c6fc7b2e46bd2f45
masbagusyutl <82576353+masbagusyutl@users.noreply.github.com>
2024-08-06 09:06:38 +0700

    Update d.py

--- d.py
+++ d.py
@@ -13,9 +13,34 @@ def read_data(filename):
             accounts.append((token, cookie))
     return accounts
 
+# Fungsi untuk login akun
+def login_account(token, cookie):
+    url = "https://drftparty.fibrum.com/auth"
+    headers = {
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate, br, zstd",
+        "Accept-Language": "en-GB,en;q=0.9,en-US;q=0.8",
+        "Cache-Control": "no-cache",
+        "Cookie": cookie,
+        "Pragma": "no-cache",
+        "Priority": "u=1, i",
+        "Referer": "https://drftparty.fibrum.com/game",
+        "Sec-Ch-Ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Microsoft Edge\";v=\"126\", \"Microsoft Edge WebView2\";v=\"126\"",
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": "\"Windows\"",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0",
+        "Token": token
+    }
+    
+    response = requests.get(url, headers=headers)
+    return response.status_code
+
 # Fungsi untuk mendapatkan informasi akun
 def info_account(token, cookie):
-    url = "https://drftparty.fibrum.com/auth"
+    url = "https://drftparty.fibrum.com/get-user"
     headers = {
         "Accept": "*/*",
         "Accept-Encoding": "gzip, deflate, br, zstd",
@@ -91,9 +116,13 @@ def main():
     
     for idx, (token, cookie) in enumerate(accounts):
         print(f"Memproses akun ke-{idx+1} dari {total_accounts}")
-        info_account(token, cookie)
-        status_code = process_account(token, cookie)
-        print(f"Status kode: {status_code}")
+        login_status = login_account(token, cookie)
+        if login_status == 200:
+            info_account(token, cookie)
+            status_code = process_account(token, cookie)
+            print(f"Status kode: {status_code}")
+        else:
+            print(f"Gagal login. Status kode: {login_status}")
         time.sleep(5)
     
     print("Semua akun telah diproses. Memulai hitung mundur 1 hari.")
